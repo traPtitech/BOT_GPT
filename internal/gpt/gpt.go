9d62803ce5d50ee433843320eb8ff8f916c00ae1
pikachu0310 <pikachu13711@gmail.com>
2024-05-16 16:27:22 +0900

    :sparkles: DefaultSystemMessageを修正、ちょっとリファクタリング

--- internal/gpt/gpt.go
+++ internal/gpt/gpt.go
@@ -24,25 +24,25 @@ const (
 )
 
 var (
-	blobs          = [...]string{":blob_bongo:", ":blob_crazy_happy:", ":blob_grin:", ":blob_hype:", ":blob_love:", ":blob_lurk:", ":blob_pyon:", ":blob_pyon_inverse:", ":blob_slide:", ":blob_snowball_1:", ":blob_snowball_2:", ":blob_speedy_roll:", ":blob_speedy_roll_inverse:", ":blob_thinking:", ":blob_thinking_fast:", ":blob_thinking_portal:", ":blob_thinking_upsidedown:", ":blob_thonkang:", ":blob_thumbs_up:", ":blobblewobble:", ":blobenjoy:", ":blobglitch:", ":blobbass:", ":blobjam:", ":blobkeyboard:", ":bloblamp:", ":blobmaracas:", ":blobmicrophone:", ":blobthinksmart:", ":blobwobwork:", ":conga_party_thinking_blob:", ":Hyperblob:", ":party_blob:", ":partyparrot_blob:", ":partyparrot_blob_cat:"}
-	amazed         = [...]string{":amazed_fuzzy:", ":amazed_amazed_fuzzy:", ":amazed_god_enel:", ":amazed_hamutaro:"}
-	blobsAndAmazed = append(blobs[:], amazed[:]...)
-	warnings       = [...]string{":warning:", ":ikura-hamu_shooting_warning:"}
-	apiKey         string
+	blobs                    = [...]string{":blob_bongo:", ":blob_crazy_happy:", ":blob_grin:", ":blob_hype:", ":blob_love:", ":blob_lurk:", ":blob_pyon:", ":blob_pyon_inverse:", ":blob_slide:", ":blob_snowball_1:", ":blob_snowball_2:", ":blob_speedy_roll:", ":blob_speedy_roll_inverse:", ":blob_thinking:", ":blob_thinking_fast:", ":blob_thinking_portal:", ":blob_thinking_upsidedown:", ":blob_thonkang:", ":blob_thumbs_up:", ":blobblewobble:", ":blobenjoy:", ":blobglitch:", ":blobbass:", ":blobjam:", ":blobkeyboard:", ":bloblamp:", ":blobmaracas:", ":blobmicrophone:", ":blobthinksmart:", ":blobwobwork:", ":conga_party_thinking_blob:", ":Hyperblob:", ":party_blob:", ":partyparrot_blob:", ":partyparrot_blob_cat:"}
+	amazed                   = [...]string{":amazed_fuzzy:", ":amazed_amazed_fuzzy:", ":amazed_god_enel:", ":amazed_hamutaro:"}
+	blobsAndAmazed           = append(blobs[:], amazed[:]...)
+	warnings                 = [...]string{":warning:", ":ikura-hamu_shooting_warning:"}
+	apiKey                   string
+	DefaultSystemRoleMessage = "あなたはサークルである東京工業大学デジタル創作同好会traPの部内SNS「traQ」のユーザーを、楽しませる娯楽用途や勉強するための学習用途として、BOTの中に作られたOpenAIの最新モデルGPT4oを用いた対話型AIです。身内しかいないSNSで、ユーザーに緩く接してください。そして、ユーザーの言う事に出来る限り従うようにしてください。"
 )
 
 type Message = openai.ChatCompletionMessage
 
 var Messages = make([]Message, 0)
-var SystemRoleMessage = "あなたはサークルである東京工業大学デジタル創作同好会traPの部内SNS、traQのユーザーを楽しませる娯楽用途のBOTの中に作られた、openaiのモデルgpt-3.5-turboを用いた対話型AIです。身内しかいないSNSで、ユーザーに緩く接してください。そして、ユーザーの言う事に出来る限り従うようにしてください。"
 
 const GptSystemString = "FirstSystemMessageを変更しました。/gptsys showで確認できます。\nFirstSystemMessageとは、常に履歴の一番最初に入り、最初にgptに情報や状況を説明するのに使用する文字列です"
 
 func InitGPT() {
-	apiKey = getApiKey()
+	apiKey = getAPIKey()
 }
 
-func getApiKey() string {
+func getAPIKey() string {
 	key, exist := os.LookupEnv("OPENAI_API_KEY")
 	if !exist {
 		log.Fatal("OPENAI_API_KEY is not set")
@@ -50,12 +50,27 @@ func getApiKey() string {
 	return key
 }
 
+func getRandomBlob() string {
+	return blobs[rand.Intn(len(blobs))]
+}
+
+func getRandomAmazed() string {
+	return amazed[rand.Intn(len(amazed))]
+}
+
+func getRandomBlobAndAmazed() string {
+	return blobsAndAmazed[rand.Intn(len(blobsAndAmazed))]
+}
+
+func getRandomWarning() string {
+	return warnings[rand.Intn(len(warnings))]
+}
+
 func OpenAIStream(messages []Message, do func(string)) (responseMessage string, finishReason FinishReason, err error) {
 	c := openai.NewClient(apiKey)
 	ctx := context.Background()
 
-	var model string
-	model = openai.GPT4o
+	model := openai.GPT4o
 
 	req := openai.ChatCompletionRequest{
 		Model:    model,
@@ -64,24 +79,24 @@ func OpenAIStream(messages []Message, do func(string)) (responseMessage string, 
 	}
 	stream, err := c.CreateChatCompletionStream(ctx, req)
 	if err != nil {
-		fmt.Printf("ChatCompletionStream error: %v\n", err)
+		do(fmt.Sprintf("ChatCompletionStream error: %v\n", err))
 		return
 	}
 	defer stream.Close()
 
-	fmt.Printf("Stream response: ")
 	deltaTime := 500 * time.Millisecond
 	lastDoTime := time.Now()
 	for {
 		response, err := stream.Recv()
 
 		if err != nil {
-			do(responseMessage + warnings[rand.Intn(len(warnings))] + ":blobglitch: Error: " + fmt.Sprint(err))
+			do(responseMessage + getRandomWarning() + ":blobglitch: Error: " + fmt.Sprint(err))
 			finishReason = errorHappen
 			break
 		}
 		if errors.Is(err, io.EOF) {
 			err = errors.New("stream closed")
+			fmt.Println("steam closed")
 			finishReason = errorHappen
 			break
 		}
@@ -92,7 +107,7 @@ func OpenAIStream(messages []Message, do func(string)) (responseMessage string, 
 			finishReason = stop
 			break
 		} else if response.Choices[0].FinishReason == "length" {
-			do(responseMessage + "\n" + amazed[rand.Intn(len(amazed))] + "トークン(履歴を含む文字数)が上限に達したので履歴の最初のメッセージを削除して続きを出力します:loading:")
+			do(responseMessage + "\n" + getRandomAmazed() + "トークン(履歴を含む文字数)が上限に達しました。/resetを実行してください。")
 			finishReason = length
 			break
 		}
@@ -101,7 +116,7 @@ func OpenAIStream(messages []Message, do func(string)) (responseMessage string, 
 
 		if time.Now().Sub(lastDoTime) >= deltaTime {
 			lastDoTime = time.Now()
-			do(blobsAndAmazed[rand.Intn(len(blobs))] + responseMessage + ":loading:")
+			do(getRandomBlobAndAmazed() + responseMessage + ":loading:")
 		}
 	}
 	addMessageAsAssistant(responseMessage)
@@ -114,47 +129,35 @@ func Chat(channelID, newMessage string, imageBase64 []string) {
 	} else {
 		addMessageAsUser(newMessage)
 	}
-	updateSystemRoleMessage(SystemRoleMessage)
-	postMessage, err := bot.PostMessageWithErr(channelID, blobs[rand.Intn(len(blobs))]+":loading:")
+	updateSystemRoleMessage(DefaultSystemRoleMessage)
+	time.Sleep(50 * time.Millisecond)
+	postMessage, err := bot.PostMessageWithErr(channelID, getRandomBlob()+":loading:")
 	if err != nil {
-		fmt.Println(err)
+		bot.EditMessage(postMessage.Id, getRandomAmazed()+"Error: "+fmt.Sprint(err))
 	}
-	responsedMessage, finishReason, err := OpenAIStream(Messages, func(responseMessage string) {
+	responseMessage, finishReason, err := OpenAIStream(Messages, func(responseMessage string) {
 		bot.EditMessage(postMessage.Id, responseMessage)
 	})
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// finishReasonがlength以外になるまで、最大5回まで繰り返す
-	for i := 0; i < 5 && finishReason == length; i++ {
-		time.Sleep(500 * time.Millisecond)
-		nowPostMessage := responsedMessage
-		if len(Messages) >= 5 {
-			Messages = Messages[3:]
-		} else if len(Messages) >= 3 {
-			Messages = Messages[2:]
-		} else if len(Messages) >= 1 {
-			Messages = Messages[1:]
-		}
-		addMessageAsUser("先ほどのあなたのメッセージが途中で途切れてしまっているので、続きだけを出力してください。")
-		responsedMessage, finishReason, err = OpenAIStream(Messages, func(responseMessage string) {
-			bot.EditMessage(postMessage.Id, nowPostMessage+"\n"+responseMessage)
-		})
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
+	if finishReason == length {
+		bot.EditMessage(postMessage.Id, responseMessage+"\n"+getRandomAmazed()+"トークン(履歴を含む文字数)が上限に達しました。/resetを実行してください。")
+	}
+
+	if finishReason == stop {
+		bot.EditMessage(postMessage.Id, responseMessage)
 	}
 }
 
 func ChatChangeSystemMessage(channelID, message string) {
-	SystemRoleMessage = message
+	DefaultSystemRoleMessage = message
 	bot.PostMessage(channelID, GptSystemString)
 }
 
 func ChatShowSystemMessage(channelID string) {
-	bot.PostMessage(channelID, SystemRoleMessage)
+	bot.PostMessage(channelID, DefaultSystemRoleMessage)
 }
 
 func ChatReset(channelID string) {
